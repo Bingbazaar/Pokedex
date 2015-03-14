c702ea1f62ce4da5a66719a4701a4b87534ff918
Dubstepper <Dubstepper@users.noreply.github.com>
2015-03-14 01:55:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-//variables
-var queryString = [],
-  query = window.location.search.substring(1),
-  vars = query.split("&");
-
-//GET url parameters
-var getUrlParams = function (){
-
-    for(i = 0; i < vars.length; i++){
-      var pair = vars[i].split("=");
-      if(typeof queryString[pair[0]] === "undefined"){
-        queryString[pair[0]] = pair[1];
-      } else if(typeof queryString[pair[0]] === "string"){
-        var arr = [queryStrings[pair[0]], pair[1]];
-        queryString[pair[0]] = pair[1];
-      } else {
-        queryString[pair[0]].push(pair[1]);
+$(function(){
+  //variables
+  var queryString = [],
+    query = window.location.search.substring(1),
+    vars = query.split("&");
+  
+  //GET url parameters
+  var getUrlParams = function (){
+  
+      for(i = 0; i < vars.length; i++){
+        var pair = vars[i].split("=");
+        if(typeof queryString[pair[0]] === "undefined"){
+          queryString[pair[0]] = pair[1];
+        } else if(typeof queryString[pair[0]] === "string"){
+          var arr = [queryStrings[pair[0]], pair[1]];
+          queryString[pair[0]] = pair[1];
+        } else {
+          queryString[pair[0]].push(pair[1]);
+        }
       }
+      return queryString;
+  } ();
+  
+  var goButton = document.getElementsByClassName('submit'),
+    currentPoke = document.getElementById('poke-input').value,
+    newUrl = 'http://dubstepper.github.io/Pokedex/?poke=' + currentPoke;
+  goButton.onclick = function(){
+    if(!currentPoke.trim().length > 0) {
+        window.history.pushState({}, 'Pokedex', newUrl); 
     }
-    return queryString;
-} ();
-
-var goButton = document.getElementsByClassName('submit'),
-  currentPoke = document.getElementById('poke-input').value,
-  newUrl = 'http://dubstepper.github.io/Pokedex/?poke=' + currentPoke;
-goButton.onclick = function(){
-  if(!currentPoke.trim().length > 0) {
-      window.history.pushState({}, 'Pokedex', newUrl); 
-  }
-};
+  };
+});
