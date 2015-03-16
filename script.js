094552617e7baf94fcfbdfba8990ce80ef1f4988
Dubstepper <Dubstepper@users.noreply.github.com>
2015-03-16 19:52:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ $(function(){
         '<img src="' + path + '">' +
         '<h1 class="poke-name">' + result["name"] + '</h1>');
     }).fail(function(){
-      $('.result').html('<h1 class="poke-name">Sorry, we couldn\'t fetch that Poke :(</h1>');
+      $('.result').html('<h1 class="poke-name">Sorry, we couldn\'t find that Poke :(</h1>');
     });
   };
 
