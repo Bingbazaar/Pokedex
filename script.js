f04372bf68674fc970d4ef2c2c8715175ac291ca
Dubstepper <thenoisymango@gmail.com>
2015-03-16 19:07:54 +0100

    hey

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ $(function(){
   var queryString = [],
     query = window.location.search.substring(1),
     vars = query.split("&");
-  
+
   //GET url parameters
   var getUrlParams = function (){
-  
+
       for(i = 0; i < vars.length; i++){
         var pair = vars[i].split("=");
         if(typeof queryString[pair[0]] === "undefined"){
@@ -20,14 +20,42 @@ $(function(){
       }
       return queryString;
   } ();
-  
-  var goButton = document.getElementById('submit');
-  
-  goButton.onclick = function(){
-    var currentPoke = document.getElementById('poke-input').value;
-    var newUrl = 'http://dubstepper.github.io/Pokedex/?poke=' + currentPoke;
-    if(currentPoke.trim().length > 0) {
-        window.history.pushState({}, 'Pokedex', newUrl); 
+
+  var goButton = document.getElementById('submit'),
+    pokeInput = document.getElementById('poke-input');
+
+    function getPoke(url, callback){
+      var req = new XMLHttpRequest();
+
+      req.onreadystatechange = function(){
+        if(req.readystate == 4 && req.status == 200) callback(req.responseText);
+      };
+      req.open('GET', url, true);
+      req.send(null);
+
     }
+
+  goButton.onclick = function(){
+
+    //loading screen
+    $('.result').html('<i class="fa fa-spinner"></i>');
+
+    var currentPoke = document.getElementById('poke-input').value,
+      path = './img/' + currentPoke + '.gif';
+    var newUrl = 'http://dubstepper.github.io/Pokedex/?poke=' + currentPoke;
+    // if(currentPoke.trim().length > 0) {
+    //     window.history.pushState({}, 'Pokedex', newUrl);
+    // }
+    $.getJSON('http://pokeapi.co/api/v1/pokemon/' + currentPoke.toString().toLowerCase(), function(result){
+      $('.intro').html('');
+      $('.poke-name').text(result["name"]);
+      $('.result').html(
+      '<img src="' + path + '">' +
+      '<h1 class="poke-name">' + result["name"] + '</h1>');
+    });
   };
+
+  $('#poke-input').keypress(function(e){
+    if(e.which == 13) goButton.click();
+  });
 });
